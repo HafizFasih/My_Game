569278e3591bd3140edf7a567e29866f278c3ac6
Muhammad Fasih <161964251+HafizFasih@users.noreply.github.com>
2024-03-25 14:13:52 +0500

    Add files via upload
    rock paper scissor game

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let computerGuessed = Math.floor(Math.random() * 3 + 1)
+
+let result:string = ""
+if(computerGuessed === 1){
+    result = "rock"
+}else if(computerGuessed === 2){
+    result = "paper"
+}
+else{
+    result = "scissor"
+}
+
+const answer = await inquirer.prompt([
+    {
+        name:"userGuessed",
+        type:"list",
+        message:"SELECT ONE OF THE FOLLOWING: ",
+        choices:["rock","paper","scissor"]
+    }
+])
+if(result === "paper" && answer.userGuessed === "rock" || result === "rock" && answer.userGuessed === "scissor" || result === "scissor" && answer.userGuessed === "paper"){
+    console.log("YOU HAVE BEEN DEFEATED.")
+    console.log(`The computer selected ${result}`)
+}else if(result === "paper" && answer.userGuessed === "scissor" || result === "rock" && answer.userGuessed === "paper" || result === "scissor" && answer.userGuessed === "rock"){
+    console.log("CONGRATULATIONS YOU WIN.");
+    console.log(`The computer selected ${result}`)
+}else if(result === "paper" && answer.userGuessed === "paper" || result === "rock" && answer.userGuessed === "rock" || result === "scissor" && answer.userGuessed === "scissor"){
+    console.log("TIE.")
+    console.log(`The computer selected ${result}`)
+}else{
+    console.log("Please choosa a valid option.");
+    
+}
